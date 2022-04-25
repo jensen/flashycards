6fe4020fa1ea9bc896d98735fd7f2674d5793fcd
Karl Jensen <karljensen@gmail.com>
2022-04-24 22:38:10 -0700

    Title update

--- src/root.tsx
+++ src/root.tsx
@@ -45,7 +45,7 @@ export const links: LinksFunction = () => [
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "flashy",
+  title: "flashycards",
   viewport: "width=device-width,initial-scale=1",
 });
 
